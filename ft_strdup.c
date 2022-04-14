3cb492d1b14555749b9cc86e57d6d8ec1ec189b8
DimaSoroko <84499772+DimaSoroko@users.noreply.github.com>
2022-04-14 10:19:31 +0200

    last

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,29 +6,30 @@
 /*   By: dsoroko <disoroko@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 17:44:41 by dsoroko           #+#    #+#             */
-/*   Updated: 2022/04/13 18:17:23 by dsoroko          ###   ########.fr       */
+/*   Updated: 2022/04/14 10:18:15 by dsoroko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-char	*ft_strstr(const char *s1)
+#include <stdio.h>
+#include <string.h>
+char *ft_strdup(const char *s1)
 {
-	char	*str;
-	int		i;
+	char *str;
 
-	i = 0;
-	while (s1[i] != '\0')
-		i++;
-	str = malloc(sizeof(char) * ft_strlen(s1) + 1);
-	if (str == NULL)
+	str = malloc(sizeof(char) * strlen(s1) + 1);
+	if (!str)
 		return (0);
-	i = 0;
-	while (s1[i] != '\0')
-	{
-		str[i] = s1[i];
-		i++;
-	}
-	str[i] = '\0';
+	strlcpy(str, s1, strlen(str));
+
+	//str[i] = '\0';
 	return (str);
+}
+
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	printf("%s\n", ft_strdup("hello"));
+	printf("%s\n", strdup("hello"));
 }
